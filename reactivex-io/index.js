94be178e1e3515d56cce3ed474aa28524d46b1d1
Greg Pfaff <greg.pfaff@razorfish.com>
2016-04-15 16:36:53 -0500

    adding answers for 5 and 6

--- reactivex-io/index.js
+++ reactivex-io/index.js
@@ -65,8 +65,7 @@ names.forEach(function(name) {
 			title: object.title
 		});
 })
-	console.log(videoAndTitlePairs);
-	return videoAndTitlePairs; 
+	console.log('videoPairs:' , videoAndTitlePairs);
 
 
 // Using MAP on the array type 
@@ -75,9 +74,7 @@ Array.prototype.map = function(projectionFunction) {
 	var results = [];
 	this.forEach(function(itemInArray) {
 	  results.push(projectionFunction(itemInArray));
-	  console.log(results);
 	});
-
 	return results;
 	console.log('results ', results);
 }
@@ -126,7 +123,49 @@ return newReleases.map(function(obj){
       };
    });
 
-console.log(newReleases);
+console.log('new releases: ', newReleases);
 
 
+var newReleases = [
+		{
+			"id": 70111470,
+			"title": "Die Hard",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 4.0,
+			"bookmark": []
+		},
+		{
+			"id": 654356453,
+			"title": "Bad Boys",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 5.0,
+			"bookmark": [{ id:432534, time:65876586 }]
+		},
+		{
+			"id": 65432445,
+			"title": "The Chamber",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 4.0,
+			"bookmark": []
+		},
+		{
+			"id": 675465,
+			"title": "Fracture",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 5.0,
+			"bookmark": [{ id:432534, time:65876586 }]
+		}
+	],
+	videos = [];
 
+  newReleases.forEach(function(obj) {
+    if(obj.rating >= 5.0) {
+     videos.push(obj); 
+    }
+  })
+
+	console.log('videos :', videos);
