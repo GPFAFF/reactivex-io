c748943ce0aecaee80003a7232bd5b79a3e28e95
Greg Pfaff <greg.pfaff@razorfish.com>
2016-04-21 22:34:11 -0500

    creating the filter function

--- reactivex-io/index.js
+++ reactivex-io/index.js
@@ -154,3 +154,16 @@ newReleases.forEach(function(obj) {
 })
 
 console.log('videos :', videos);
+
+// Using filter()
+
+Array.prototype.filter = function(predicateFunction) {
+    var results = [];
+    this.forEach(function(itemInArray) {
+      if(predicateFunction(itemInArray)) {
+        results.push(itemInArray);
+      }
+    });
+
+    return results;
+};
